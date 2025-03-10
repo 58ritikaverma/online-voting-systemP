0ef32176da282e2c88e727cff20e0c9883b7a872
Ritika Verma <rv854031@gmail.com>
2025-03-10 22:09:40 +0530

    commit3

--- online-voting-system-backend/db.js
+++ online-voting-system-backend/db.js
@@ -1,11 +1,29 @@
+// const mongoose = require("mongoose");
+
+// // Data Base URL
+// const monngodb_url = `mongodb://localhost:27017/voting_db`;
+
+// mongoose.connect(monngodb_url, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+//     console.log("mongoDB is connected");
+// }).catch((error)=>{
+//     console.log("mongoDB not connected");
+//     console.log(error);
+// });
+
 const mongoose = require("mongoose");
 
-// Data Base URL
-const monngodb_url = `mongodb://localhost:27017/voting_db`;
+mongoose.set("strictQuery", true); // Set strictQuery to avoid deprecation warnings
 
-mongoose.connect(monngodb_url, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
-    console.log("mongoDB is connected");
-}).catch((error)=>{
-    console.log("mongoDB not connected");
-    console.log(error);
+const mongodb_url = "mongodb://localhost:27017/voting_db";
+
+mongoose.connect(mongodb_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // Increase timeout (default is 30000ms)
+  })
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:",error);
 });
